20f21835433b765c3d8ed7c7ee03fd1052bcd081
Tiakumzuk <tiakumzukimsong@gmail.com>
2023-05-29 19:05:44 +0530

    wifi completed

--- wifi/Wifi.py
+++ wifi/Wifi.py
@@ -3,12 +3,12 @@ import time
 from PIL import Image
 
 def click_menu():
-	a = pyautogui.locateCenterOnScreen("menu.png",grayscale=True,confidence=0.85)
+	a = pyautogui.locateCenterOnScreen("menu.png",grayscale=True,confidence=0.5)
 
 	pyautogui.click(a)
 	# not_connected()
-	wifi_off()
-
+	# wifi_off()
+	# main()
 
 def not_connected():
 	# reference_image = Image.open('/home/tia/Desktop/gui/wifi/wifi_not_connected.png')
@@ -43,7 +43,7 @@ def not_connected():
 
 
 def wifi_off():
-	off = pyautogui.locateCenterOnScreen("wifi_off.png",grayscale=True,confidence=.85)
+	off = pyautogui.locateCenterOnScreen("wifi_off.png",grayscale=True,confidence=.1)
 
 	if off is not None:
 		return True
@@ -51,12 +51,60 @@ def wifi_off():
 		return False
 	# print(off)
 
+def wifi_connected():
+	connected  = pyautogui.locateCenterOnScreen("wifi_connected.png", grayscale=True, confidence=.85)
+	if connected is not None:
+		return True	
+	else:
+		return False
 
+	# print(connected)
+
+def onconnecting():
+	onconnecting = pyautogui.locateCenterOnScreen("wifi_connecting.png",grayscale=True,confidence=.85)
+	if onconnecting is not None:
+		return True	
+	else:
+		return False
 
 def main():
+
+	click_menu()
+
 	if wifi_off():
-		off_wifi = pyautogui.locateCenterOnScreen("wifi_off.png",grayscale=True,confidence=.85)
+		settings = pyautogui.locateCenterOnScreen("settings.png",grayscale=True,confidence=.85)
+		pyautogui.click(settings)
+		time.sleep(2)
+		turnon = pyautogui.locateCenterOnScreen("turnon.png", grayscale=True, confidence=.85)
+		pyautogui.click(turnon)
+		time.sleep(10)
+		
+
+	elif onconnecting():
+		pass
+
+
+	elif not_connected():
+
+		settings = pyautogui.locateCenterOnScreen("settings.png",grayscale=True,confidence=.85)
+
+
+		pyautogui.click(settings)
+		time.sleep(2)
+
+		tiakumzuk_wifi = pyautogui.locateCenterOnScreen("tiakumzuk.png",grayscale=True,confidence=.85)
+
+		pyautogui.click(tiakumzuk_wifi)
+		time.sleep(10)
 		
 
 
-click_menu()
+	elif wifi_connected():
+		pass
+
+	
+		
+
+# click_menu()
+main()
+# wifi_off()
